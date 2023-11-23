39bc1a8e9c83d43860282f9ce3518e16fe5048e2
weijie24559 <30461258+weijie24559@users.noreply.github.com>
2023-11-23 16:50:42 +0800

    Update application.py

--- helloworld/application.py
+++ helloworld/application.py
@@ -8,11 +8,11 @@ application = Flask(__name__)
 @application.route('/', methods=['GET'])
 def get():
     #return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
-    return Response(json.dumps({'Output': 'Hello World from Codestar 123'}), mimetype='application/json', status=200)
+    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
 
 @application.route('/', methods=['POST'])
 def post():
-    return Response(json.dumps({'Output': 'Hello World 123'}), mimetype='application/json', status=200)
+    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
 
 if __name__ == '__main__':
     flaskrun(application)
